f688bb6f810548c57481bbc659b411a86912a4ff
PiotrKruk99-d <biuro@liberezo.pl>
2022-11-20 17:54:40 +0100

    Database.Migrate() not launched when testing

--- webApi/DataClasses/DataContext.cs
+++ webApi/DataClasses/DataContext.cs
@@ -20,7 +20,8 @@ public class DataContext : DbContext
         if ((optionsForTests is null) && !Directory.Exists(Path.GetDirectoryName(DbPath)))
             throw new Exception("'Data' folder for database file not exists.");
 
-        this.Database.Migrate();
+        if (optionsForTests is null)
+            this.Database.Migrate();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
